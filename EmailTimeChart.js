b8edbf5ba69eed75a448403fe0ae083ac88ea244
Mac Carter <harlow44@hotmail.com>
2016-09-14 03:12:18 -0500

    Ready to plot data, added libs to local
    Ready to plot data, just need to figure that out.

--- EmailTimeChart.js
+++ EmailTimeChart.js
@@ -1,67 +1,129 @@
+var d;
+
+let findHourTime = (time) => {
+    time = String(time);
+    var matches = time.match(/(\d+):(\d+)/);
+    
+    var hourTime = 0;
+    hourTime += parseInt(matches[0],10); 
+    
+    var timeMinuite = parseInt(matches[1],10);
+    hourTime +=  timeMinuite/60;
+    
+    if(time.toLowerCase().includes("pm")){
+        hourTime += 12;
+    }
+    return hourTime;
+};
+
 let EmailTimeChart = () => {
-    var margin = {top: 20, right: 20, bottom: 70, left: 40},
-    width = 600 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+    var data = []
 
-  // Parse the date / time
-    var	parseDate = d3.time.format("%Y-%m").parse;
-    var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
-    var y = d3.scale.linear().range([height, 0]);
+    d3.text("emailData.csv", function(emailData) {
+        
+        let allRows = d3.csvParseRows(emailData)
+            .slice(1, d3.csvParseRows(emailData).lenght);
 
-    var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
-    .tickFormat(d3.time.format("%Y-%m"));
+        let dates = allRows.map((emailData) => {return emailData[0];});
+        console.log(dates);
+        let uniqueDates = Array.from(new Set(dates));
+        console.log(uniqueDates);
+        
+        let times = allRows.map((emailData) => {return findHourTime(emailData[1]);});
+        console.log(times);
 
-    var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10);
+        d = uniqueDates.map((uniqueDate) => {
+            var count = 0;
+            var totalTime = 0;
+                allRows.map((emailData) => { 
+                    if (emailData[0] == uniqueDate) {
+                        count++;
+                    }
+                });
+                console.log(count);
+                allRows.map((emailData) => { 
+                    if (emailData[0] == uniqueDate) {
+                        totalTime += findHourTime(emailData[1]);
+                    }
+                });
 
-    var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                totalTime = totalTime/count;
+                console.log(totalTime);
+                var returnVariable = [uniqueDate,totalTime,count];
+                return returnVariable;
 
-    d3.csv("bar-data.csv", function(error, data) {
-
-        data.forEach(function(d) {
-            d.date = parseDate(d.date);
-            d.value = +d.value;
         });
-  	
-    x.domain(data.map(function(d) { return d.date; }));
-    y.domain([0, d3.max(data, function(d) { return d.value; })]);
+    });
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-      .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("dx", "-.8em")
-        .attr("dy", "-.55em")
-        .attr("transform", "rotate(-90)" );
+     data = [[5,3,22], [10,17,4], [15,4,5], [2,8,8]];
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-      .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Value ($)");
+        let margin = {top: 50, right: 50, bottom: 50, left: 50}
+      , width = 960 - margin.left - margin.right
+      , height = 500 - margin.top - margin.bottom;
 
-    svg.selectAll("bar")
+    let x = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d) { return d[0]; })])
+        .range([ 0, width ]);
+    
+    let y = d3.scaleLinear()
+        .domain([0, d3.max(data, function (d) {
+            return 24;
+        })])
+        .range([height, 0]);
+ 
+
+
+    let chart = d3.select('#EmailTimeChart')
+        .append('svg')
+        .attr('preserveAspectRatio',"xMidYMid meet")
+        .attr('viewBox',`${0} ${0} ${width + margin.right + margin.left} ${height + margin.top + margin.bottom}`)
+        .attr('class', 'chart')
+
+    let EmailGraph = chart.append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('class', 'EmailGraph')   
+        
+
+
+    // draw the x axis
+    let xAxis = d3.axisBottom()
+        .scale(x);
+
+    EmailGraph.append('g')
+        .attr('transform', 'translate(0,' + height + ')')
+        .attr('class', 'EmailGraph axis date')
+        .call(xAxis);
+
+
+
+    // draw the y axis
+    let yAxis = d3.axisLeft()
+        .scale(y)
+        .ticks(12);
+
+    EmailGraph.append('g')
+        .attr('transform', 'translate(0,0)')
+        .attr('class', 'EmailGraph axis date')
+        .call(yAxis);
+
+
+
+    let g = EmailGraph.append("g"); 
+    
+    g.selectAll("scatter-dots")
         .data(data)
-      .enter().append("rect")
-        .style("fill", "steelblue")
-        .attr("x", function(d) { return x(d.date); })
-        .attr("width", x.rangeBand())
-        .attr("y", function(d) { return y(d.value); })
-        .attr("height", function(d) { return height - y(d.value); });
+        .enter().append("circle")
+            .attr("cx", function (d,i) { return x(d[0]); } )
+            .attr("cy", function (d) { return y(d[1]); } )
+            .attr("r",  function (d) { return d[2]; } )
 
-  });
+             // Add the points!
+    g.selectAll(".point")
+        .data(data)
+    .enter().append("triangle-up")
+        .attr("class", "point")
+        .attr("transform", function(d) { return "translate(" + x(d[0]) + "," + y(d[1]) + ")"; });
+
 };
